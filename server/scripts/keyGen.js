0dc82a64f69312032f14f6dba322c84e713e5e43
Alejandro Silván <alejandro.silvan.dev@pm.me>
2024-02-02 18:36:05 +0100

    move const

--- server/scripts/keyGen.js
+++ server/scripts/keyGen.js
@@ -1,7 +1,7 @@
-function keyGenerator() {
-    const { secp256k1 } = require('ethereum-cryptography/secp256k1')
-    const { toHex } = require('ethereum-cryptography/utils')
+const { secp256k1 } = require('ethereum-cryptography/secp256k1')
+const { toHex } = require('ethereum-cryptography/utils')
 
+function keyGenerator() {
     const privateKey = secp256k1.utils.randomPrivateKey()
     const publicKey = secp256k1.getPublicKey(privateKey)
 
